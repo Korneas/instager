2eefc6e702032dd68a62f73a268fee92047e869a
Arturo Gomez <gomezardi@hotmail.com>
2016-09-27 21:21:10 -0500

    Comentado logica / Se retiran elemento innecesarios

--- src/instager/Logica.java
+++ src/instager/Logica.java
@@ -1,27 +1,23 @@
 package instager;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 import processing.core.*;
 
 public class Logica {
 
+	//Creacion de elementos necesarios
 	private PApplet app;
 	private PImage fondo, interfaz, tools, botonAs, botonAn, botonDs, botonDn;
 	private Imagen img;
 	private boolean full, asc, des = true;
 	private ArrayList<Imagen> fotos;
-	private ListIterator<Imagen> iter;
 	private int pos;
 
+	
 	public Logica(PApplet app) {
 		this.app = app;
 		fotos = new ArrayList<Imagen>();
@@ -33,20 +29,28 @@ public class Logica {
 		botonDn = app.loadImage("botonIlegidoD.png");
 		botonDs = app.loadImage("botonElegidoD.png");
 		crearImagenes();
-		iter = fotos.listIterator();
 		img = fotos.get(pos);
 		pos = 0;
 		img.lugar(500, 258);
 	}
 
+	//Metodo para ejecutar el código
 	public void ejecutar() {
+		
+		//Imagen principal se actualiza constantemente
 		img = fotos.get(pos);
+		
+		//Modo de imagen con punto de alineación en el centro
 		app.imageMode(PApplet.CENTER);
+		
+		//Si no esta en pantalla completa, entonces muestre la interfaz
 		if (full == false)
 			app.image(fondo, app.width / 2, app.height / 2);
 
+		//IMAGEN PRINCIPAL
 		img.rotar();
 
+		//Si no esta en pantalla completa, entonces muestre la interfaz
 		if (full == false) {
 			app.image(interfaz, app.width / 2, app.height / 2);
 			if (asc == true) {
@@ -80,16 +84,20 @@ public class Logica {
 			}
 		}
 
+		//Herramientas de edicion
 		app.image(tools, app.width / 2, app.height / 2);
 	}
 
 	public void click() {
 
+		//Zona sensible de reducción
 		if (zonaSensible(0)) {
 			full = false;
 			img.lugar(500, 258);
 			img.devolver();
 		}
+		
+		//Zona sensible fullScreen
 		if (zonaSensible(1)) {
 			full = true;
 			int escalaX = img.getEscalaX();
@@ -101,6 +109,8 @@ public class Logica {
 				img.lugar(500, 350);
 			}
 		}
+		
+		//Zona sensible Zoom in
 		if (zonaSensible(2)) {
 			if (img.getEscalaX() < 950) {
 				int escalaX = img.getEscalaX();
@@ -108,6 +118,8 @@ public class Logica {
 				img.escalar(escalaX + (int) (escalaX * 0.1), escalaY + (int) (escalaY * 0.1));
 			}
 		}
+		
+		//Zona sensible Zoom out
 		if (zonaSensible(3)) {
 
 			if (img.getEscalaX() > 200) {
@@ -116,13 +128,18 @@ public class Logica {
 				img.escalar(escalaX - (int) (escalaX * 0.1), escalaY - (int) (escalaY * 0.1));
 			}
 		}
+		
+		//Zona sensible girar derecha
 		if (zonaSensible(4)) {
 			img.setAngulo(img.getAngulo() + 90);
 		}
+		
+		//Zona sensible girar izquierda
 		if (zonaSensible(5)) {
 			img.setAngulo(img.getAngulo() - 90);
 		}
 
+		//Boton para organizar por nombre del archivo dependiendo de si el boton esta ascendente o descendetne
 		if (zonaMouse(24, 90, 507, 545)) {
 			if (asc == true) {
 				Collections.sort(fotos, new ComparadorNombreAsc());
@@ -131,6 +148,7 @@ public class Logica {
 			}
 		}
 
+		//Boton para organizar por tipo del archivo dependiendo de si el boton esta ascendente o descendetne
 		if (zonaMouse(114, 180, 507, 545)) {
 			if (asc == true) {
 				Collections.sort(fotos, new ComparadorTipoAsc());
@@ -139,6 +157,7 @@ public class Logica {
 			}
 		}
 
+		//Boton para organizar por tamaño horizontal del archivo dependiendo de si el boton esta ascendente o descendetne
 		if (zonaMouse(205, 271, 507, 545)) {
 			if (asc == true) {
 				Collections.sort(fotos, new ComparadorSizeAsc());
@@ -147,17 +166,19 @@ public class Logica {
 			}
 		}
 
+		//Boton ascendente
 		if (zonaMouse(820, 886, 507, 545)) {
 			asc = true;
 			des = false;
 		}
 
+		//Boton descendente
 		if (zonaMouse(911, 977, 507, 545)) {
 			des = true;
 			asc = false;
 		}
 
-		// Zona de cambio de imagenes
+		// Zona de cambio de imagenes, izquierda - derecha
 		if (zonaMouse(68, 148, 228, 300)) {
 			if (pos - 1 > -1) {
 				pos--;
@@ -170,7 +191,9 @@ public class Logica {
 			}
 		}
 	}
-
+	
+	
+	//Metodo que solicita un valor para crear una zona sensible, utilizada para las herramientas
 	public boolean zonaSensible(int i) {
 		if (app.mouseX > (i * 92) + 225 && app.mouseX < ((i + 1) * 92) + (225) && app.mouseY > 0 && app.mouseY < 52) {
 			return true;
@@ -178,13 +201,17 @@ public class Logica {
 		return false;
 	}
 
+	//Metodo para crear una zona sensible con el mouse
 	public boolean zonaMouse(int x1, int x2, int y1, int y2) {
 		if (app.mouseX > x1 && app.mouseX < x2 && app.mouseY > y1 && app.mouseY < y2) {
 			return true;
 		}
 		return false;
 	}
+	
 
+	//Metodo para crear las imagenes con un for basado en el contenido de la carpeta "imagenes", utilizando el
+	//metodo agregarImagen
 	public void crearImagenes() {
 
 		String directorioImagenes = "./data/imagenes/";
@@ -201,6 +228,7 @@ public class Logica {
 
 	}
 
+	//Metodo para crear un nuevo Object Imagen con caracteristicas de nombre, tipo e imagen
 	public void agregarImagen(File files) {
 
 		String nombre = files.getName();
